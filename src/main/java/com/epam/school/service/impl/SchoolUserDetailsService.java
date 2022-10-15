9a84adf96dad110034c5a99d3088b2651815e227
Valeria Cervantes <91236593+ValeriaCervantes19@users.noreply.github.com>
2022-10-14 20:51:45 -0500

    Log in implemented and working

--- src/main/java/com/epam/school/service/impl/SchoolUserDetailsService.java
+++ src/main/java/com/epam/school/service/impl/SchoolUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.epam.school.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
